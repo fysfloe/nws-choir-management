2ee8217387ddcd10b0d4e8d949ee198685d6fe45
Florian Csizmazia <florian.csizmazia@gugler.at>
2018-01-07 15:17:41 +0100

    named countries table

--- database/migrations/2017_12_10_141556_charify_countries_table.php
+++ database/migrations/2017_12_10_141556_charify_countries_table.php
@@ -13,11 +13,11 @@ class CharifyCountriesTable extends Migration {
 	{
             Schema::table('countries', function($table)
             {
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY country_code CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY iso_3166_2 CHAR(2) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY iso_3166_3 CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY region_code CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY sub_region_code CHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY country_code CHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY iso_3166_2 CHAR(2) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY iso_3166_3 CHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY region_code CHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY sub_region_code CHAR(3) NOT NULL DEFAULT ''");
             });
         }
 	/**
@@ -29,11 +29,11 @@ class CharifyCountriesTable extends Migration {
 	{
             Schema::table('countries', function($table)
             {
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY country_code VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY iso_3166_2 VARCHAR(2) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY iso_3166_3 VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY region_code VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . 'countries' . " MODIFY sub_region_code VARCHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY country_code VARCHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY iso_3166_2 VARCHAR(2) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY iso_3166_3 VARCHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY region_code VARCHAR(3) NOT NULL DEFAULT ''");
+                DB::statement("ALTER TABLE countries MODIFY sub_region_code VARCHAR(3) NOT NULL DEFAULT ''");
             });
 	}
 
