5ecb80f17b3f11813f05219fb060d327730cf35e
Florian Csizmazia <florian.csizmazia@gugler.at>
2018-01-07 15:31:12 +0100

    reset config to use env variables

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://chorganizer.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
--- config/database.php
+++ config/database.php
@@ -48,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host, // env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database, // env('DB_DATABASE', 'forge'),
-            'username' => $username, // env('DB_USERNAME', 'forge'),
-            'password' => $password, // env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
